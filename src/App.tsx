57bbae6c6bdbc6960116d694aa88606e366f7d73
Solar5503 <spidervvv@mail.ru>
2022-05-15 16:15:12 +0300

    add reusable list

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import { IUser } from './types/types';
+import List from './components/List';
+import TodoItem from './components/TodoItem';
+import UserItem from './components/UserItem';
+import { ITodo, IUser } from './types/types';
 
 const App = () => {
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: 'Ulbi TV',
-      email: 'adf@mail.ru',
-      address: { city: 'Moskow', street: 'Lenina', zipcode: '123' },
-    },
-    {
-      id: 2,
-      name: 'Timur',
-      email: 'zhj@mail.ru',
-      address: { city: 'Kaliningrad', street: 'Pushkina', zipcode: '127' },
-    },
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  useEffect(() => {
+    fetchUsers();
+    fetchTodos();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setUsers(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        'https://jsonplaceholder.typicode.com/todos?_limit=10'
+      );
+      setTodos(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
     <div>
       <Card width="200px" height="200px" variant={CardVariant.outlined}>
         <button>Кнопка</button>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   );
 };
