a1a292f0b34c6c2c73f7be6ab474aa302c04a0e4
Solar5503 <spidervvv@mail.ru>
2022-05-15 17:32:59 +0300

    add event types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
+import EventsExample from './components/EventsExample';
 import List from './components/List';
 import TodoItem from './components/TodoItem';
 import UserItem from './components/UserItem';
@@ -38,6 +39,7 @@ const App = () => {
 
   return (
     <div>
+      <EventsExample />
       <Card width="200px" height="200px" variant={CardVariant.outlined}>
         <button>Кнопка</button>
       </Card>
